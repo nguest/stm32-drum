ee7e47ac882ea47ac23c7dca7043000872d182c3
nguest <nguest@workwithopal.com>
2019-03-23 16:27:59 -0700

    seems we CAN loop, it is fast enough

--- STM32-drum.ino
+++ STM32-drum.ino
@@ -1,7 +1,6 @@
 #include "CircularBuffer.h"
 #include "wavetables.h"
 
-
 #define AUDIO_CHANNEL_1_PIN PB8
 #define AUDIO_PWM_TIMER 4
 // The timer used for running the audio update loop. NOTE: Timer 3 appears to clash with SPI DMA transfers under some circumstances
@@ -20,7 +19,7 @@ HardwareTimer audio_pwm_timer(AUDIO_PWM_TIMER);
 void setup() {
    Serial.begin(115200); 
 
-  // audio update timer is running at 16kHz or whatever, but audio_pwm_timer is the thing that is doing the output at (probably) full speed
+  // audio update timer is running at 16kHz or whatever, but audio_pwm_timer is the thing that is doing the output at/near full speed
   audio_update_timer.pause();
   audio_update_timer.setPeriod(1000000UL / AUDIO_RATE);
   audio_update_timer.setChannel1Mode(TIMER_OUTPUT_COMPARE);
@@ -34,7 +33,7 @@ void setup() {
          
   #if MAX_CARRIER_FREQ < (AUDIO_RATE * 5)
     // Generate as fast a carrier as possible
-    audio_pwm_timer.setPrescaleFactor(10);
+    audio_pwm_timer.setPrescaleFactor(1);
   #else
     // No point in generating arbitrarily high carrier frequencies. In fact, if
     // there _is_ any headroom, give the PWM pin more time to swing from HIGH to LOW and BACK, cleanly
@@ -57,7 +56,7 @@ uint8_t RingWrite = 0;
 uint8_t RingRead = 0;
 volatile uint8_t RingCount = 0;
 
-const uint8_t NUM_SAMPLES = 2;
+const uint8_t NUM_SAMPLES = 3;
 uint16_t sampleCount[NUM_SAMPLES];
 uint16_t samplePointer[NUM_SAMPLES];
 
@@ -70,15 +69,15 @@ const unsigned char pattern[16] = {
   B01000001,
   B00000010,
   B01000001,
-  B00000000,
+  B00000100,
   B00100010,
-  B00000000,
-  B00100000,
+  B00000100,
+  B00100100,
   B00000000,
   B00000001,
   B00000000,
   B00000011,
-  B00000001,
+  B00000101,
   B00000001,
   B00000011,
   B00000001,
@@ -101,28 +100,25 @@ void play() {
 
   /* -------sample buffer write------------ */
 
-  int16_t sampleTotal;
+  int8_t sampleTotal;
   uint8_t stepCount = 0;
   long tempoCount = 1;
   sampleCount[NUM_SAMPLES] = { 0 };
 
   while(1) { 
-    if (RingCount < BUFFERSIZE_M1) {
-      if (sampleCount[0]) {
-        sampleTotal = (int)wavetables[0][samplePointer[0]] - HALF_AUDIO_BITS;
-        samplePointer[0]++;
-        sampleCount[0]--;
+    if (RingCount < BUFFERSIZE_M1) { // if space in ringbuffer
+      for (uint8_t i = 0; i < NUM_SAMPLES; i++) {
+        if (sampleCount[i]) {
+          sampleTotal = (int)wavetables[i][samplePointer[i]] - HALF_AUDIO_BITS; // 
+          samplePointer[i]++;
+          sampleCount[i]--;
+        }      
       }
-      if (sampleCount[1]) {
-        sampleTotal = (int)wavetables[1][samplePointer[1]] - HALF_AUDIO_BITS;
-        samplePointer[1]++;
-        sampleCount[1]--;
-      }
-      // hard clip
-      if (sampleTotal < -HALF_AUDIO_BITS-1)
-        sampleTotal = -HALF_AUDIO_BITS-1;
-      if (sampleTotal > HALF_AUDIO_BITS-1)
-        sampleTotal = HALF_AUDIO_BITS-1;     
+      // hard clip - must be faster than `constrain()`?
+      if (sampleTotal < -HALF_AUDIO_BITS)
+        sampleTotal = -HALF_AUDIO_BITS;
+      if (sampleTotal > HALF_AUDIO_BITS)
+        sampleTotal = HALF_AUDIO_BITS;
       Ringbuffer[RingWrite] = sampleTotal + HALF_AUDIO_BITS;
       RingWrite++;
       RingCount++;
@@ -137,14 +133,12 @@ void play() {
       // reset stepCount at end of pattern
       if (stepCount > patternLength) stepCount = 0;
       // read the pattern bytes, each one triggers a sample
-      if (trigger & 1) {
-        samplePointer[0] = 0;
-        sampleCount[0] = wavetableLengths[0]; // number of bytes in sample
-      }
-      if (trigger & 2) {
-        samplePointer[1] = 0;
-        sampleCount[1] = wavetableLengths[1]; // number of bytes in sample
-      }
+      for (uint8_t i = 0; i < NUM_SAMPLES; i++) {
+        if (trigger & 1<<i) {
+          samplePointer[i] = 0;
+          sampleCount[i] = wavetableLengths[i]; // number of bytes in sample
+        }
+      } 
     }
   }
   
