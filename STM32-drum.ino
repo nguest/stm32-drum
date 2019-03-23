8a5ee462bf7ff0b7b69d0085aed01209c7d4a402
nguest <nguest@workwithopal.com>
2019-03-23 11:54:03 -0700

    add second sample and arraypointers to sample array

--- STM32-drum.ino
+++ STM32-drum.ino
@@ -11,7 +11,7 @@ HardwareTimer audio_update_timer(AUDIO_UPDATE_TIMER);
 HardwareTimer audio_pwm_timer(AUDIO_PWM_TIMER);
 
 #define AUDIO_BITS 8
-#define HALF_AUDIO (1 << AUDIO_BITS)/2
+#define HALF_AUDIO_BITS (1 << AUDIO_BITS)/2
 #define MAX_CARRIER_FREQ (F_CPU / (1 << AUDIO_BITS)) // e.g 72M / 256 for 8 bit = 281250. 256 = 1 leftshift 8
 #define AUDIO_RATE 22050
 
@@ -51,13 +51,15 @@ void setup() {
 //--------- Ringbuffer parameters ----------
 
 const uint8_t BUFFERSIZE = 256;
+const uint8_t BUFFERSIZE_M1 = BUFFERSIZE - 1;
 uint8_t Ringbuffer[256];
 uint8_t RingWrite = 0;
 uint8_t RingRead = 0;
 volatile uint8_t RingCount = 0;
 
-uint16_t sampleCount;
-uint16_t samplePointer;
+const uint8_t NUM_SAMPLES = 2;
+uint16_t sampleCount[NUM_SAMPLES];
+uint16_t samplePointer[NUM_SAMPLES];
 
 //--------- Sequencer parameters ----------
 
@@ -65,25 +67,25 @@ long tempo = 1000000;
 const unsigned char patternLength = 15;
 
 const unsigned char pattern[16] = {
-  B01000000,
+  B01000001,
+  B00000010,
+  B01000001,
   B00000000,
-  B01000000,
+  B00100010,
   B00000000,
   B00100000,
   B00000000,
-  B00100000,
-  B00000000,
   B00000001,
   B00000000,
+  B00000011,
   B00000001,
   B00000001,
-  B00000001,
-  B00000001,
+  B00000011,
   B00000001,
   B00000001,
 };
 
-//--------- pwmAudioOutput ----------
+//--------- pwmAudioOutput ------//
 
 static void pwmAudioOutput() {
   if (RingCount) { //If entry in FIFO..
@@ -92,48 +94,65 @@ static void pwmAudioOutput() {
   }
 }
 
-//--------- Main Loop ----------
-//
-void loop() {   
-   play();
-}
+//--------- Write Buffer and sequence ----------//
 
 void play() {
+  // everything here optimized for processing speed not code quality
+
+  /* -------sample buffer write------------ */
+
   int16_t sampleTotal;
-  sampleCount = 0;
   uint8_t stepCount = 0;
   long tempoCount = 1;
+  sampleCount[NUM_SAMPLES] = { 0 };
+
   while(1) { 
-  //sampleCount = sizeof(kick);
-    if (RingCount < 255) {
-      if (sampleCount) {
-        sampleTotal = (int)kick[samplePointer];
-        samplePointer++;
-        sampleCount--;
+    if (RingCount < BUFFERSIZE_M1) {
+      if (sampleCount[0]) {
+        sampleTotal = (int)wavetables[0][samplePointer[0]] - HALF_AUDIO_BITS;
+        samplePointer[0]++;
+        sampleCount[0]--;
+      }
+      if (sampleCount[1]) {
+        sampleTotal = (int)wavetables[1][samplePointer[1]] - HALF_AUDIO_BITS;
+        samplePointer[1]++;
+        sampleCount[1]--;
       }
       // hard clip
-  //    if (sampleTotal < -HALF_AUDIO-1)
-  //      sampleTotal = -HALF_AUDIO-1;
-  //    if (sampleTotal > HALF_AUDIO-1)
-  //      sampleTotal = HALF_AUDIO-1;     
-      Ringbuffer[RingWrite] = sampleTotal;
+      if (sampleTotal < -HALF_AUDIO_BITS-1)
+        sampleTotal = -HALF_AUDIO_BITS-1;
+      if (sampleTotal > HALF_AUDIO_BITS-1)
+        sampleTotal = HALF_AUDIO_BITS-1;     
+      Ringbuffer[RingWrite] = sampleTotal + HALF_AUDIO_BITS;
       RingWrite++;
       RingCount++;
     }
 
-/* ----------------------------- */
+  /* -------sequencer------------ */
+
     if (!(tempoCount--)) { // every "tempo" ticks, do the thing
       tempoCount = tempo; // set it back to the tempo ticks
       uint8_t trigger = pattern[stepCount++]; //
-  
+
+      // reset stepCount at end of pattern
       if (stepCount > patternLength) stepCount = 0;
       // read the pattern bytes, each one triggers a sample
       if (trigger & 1) {
-        samplePointer = 0;
-        sampleCount = sizeof(kick); // number of bytes in sample
+        samplePointer[0] = 0;
+        sampleCount[0] = wavetableLengths[0]; // number of bytes in sample
+      }
+      if (trigger & 2) {
+        samplePointer[1] = 0;
+        sampleCount[1] = wavetableLengths[1]; // number of bytes in sample
       }
     }
-//    playSequencer(tempoCount, stepCount);
   }
+  
 /* ----------------------------- */
+}
+
+//--------- Main Loop ----------//
+
+void loop() {   
+   play();
 }
