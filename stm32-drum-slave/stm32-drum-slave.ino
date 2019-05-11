59e9765043895ccb0907cb3e3b6e9b6380b35091
nguest <nguest@workwithopal.com>
2019-05-11 15:51:35 -0700

    BPM slave working better now with joystick y-axis

--- stm32-drum-slave/stm32-drum-slave.ino
+++ stm32-drum-slave/stm32-drum-slave.ino
@@ -2,7 +2,7 @@
 #include <SPI.h>
 
 #define joystickXPin A2
-#define joystickYPin A3
+#define joystickYPin A0
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 //U8G2_SH1106_128X64_NONAME_1 k_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
@@ -36,8 +36,8 @@ void setup (void) {
   pinMode(MISO, OUTPUT);
   pinMode(2, INPUT);
 
-  pinMode(joystickXPin, INPUT);
-  pinMode(joystickYPin, INPUT);
+//  pinMode(joystickXPin, INPUT);
+//  pinMode(joystickYPin, INPUT);
 
   SPCR |= _BV(SPE); // turn on SPI in slave mode
   SPCR |= _BV(SPIE);  // turn on interrupts
@@ -109,13 +109,14 @@ void renderPattern(void) {
 
 void renderStep(void) {
   // display stepCount number
-  char tmp_string[8];
-  itoa(buffer[0], tmp_string, 10);
-  u8g2.drawStr(0, 6, tmp_string);
+  char str1[8];
+  itoa(buffer[0], str1, 10);
+  u8g2.drawStr(0, 6, str1);
 
-  char tmp_string2[3];
-  itoa(tempo, tmp_string2, 10);
-  u8g2.drawStr(80, 6, tmp_string2);
+  readTempo();
+  char str2[3];
+  itoa(tempo, str2, 10);
+  u8g2.drawStr(80, 6, str2);
 
   u8g2.setDrawColor(2); // XOR
   u8g2.drawBox((buffer[0])*8,offsetY,8,54);
@@ -156,20 +157,17 @@ void renderCursor(int dx, int dy) {
 
 volatile int tDelay;
 
-void renderTempo() {
+void readTempo() {
 
   if (tDelay > 10) {
-    Serial.println(MODE);
+    //Serial.println(MODE);
 
     //u8g2.clearBuffer();
-    tempo = constrain(tempo + joystick.x, 30, 50);
-    Serial.print("Tempo ");Serial.println(joystick.x);
-    char tmp_string[4];
-    itoa(tempo, tmp_string, 10);
-//    u8g2.drawStr(80, 6, tmp_string);
-//    u8g2.sendBuffer();
+    tempo = constrain(tempo + joystick.y, 30, 50);
 
     joystick.x = 0;
+    joystick.y = 0;
+
     tDelay = 0;
   }
   tDelay++;
@@ -185,7 +183,7 @@ int readJoystick(int wait) {
     int x = analogRead(joystickXPin);
     int y = analogRead(joystickYPin);
   
-    //Serial.println(y);
+    //Serial.print(x);Serial.print(" ");Serial.println(y);
     uint8_t threshold = 100;
 
     if (x < threshold) {
@@ -195,17 +193,13 @@ int readJoystick(int wait) {
       joystick.x = -1;
     }
     if (y < threshold) {
-      joystick.y = 1;
-    }
-    if (y > 1023 - threshold) {
       joystick.y = -1;
     }
-   // Serial.print(joystick.x);Serial.print(F(" J "));Serial.println(joystick.y);
-    //renderCursor(joystick.x,joystick.y);
+    if (y > 1023 - threshold) {
+      joystick.y = 1;
+    }
     jDelay = 0;
   }
-//  joystick.x = 0;
-//  joystick.y = 0;
 
   jDelay++;
 
@@ -221,13 +215,11 @@ void loop (void) {
   //Serial.println();
 
   
-     //Serial.println(x);
   if (MODE == 0) {
     readJoystick(10000);
   }
   if (MODE == 1) {
     readJoystick(10);
-    renderTempo();
     renderPattern();
   }
 
